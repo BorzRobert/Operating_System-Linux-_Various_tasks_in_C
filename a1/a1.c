a3e6c29b6ea191da10b17e7054b57f66b769c07c
BorzRobert <borzrobert2002@gmail.com>
2023-04-01 17:33:22 +0300

    Third version

--- a1/a1.c
+++ a1/a1.c
@@ -35,6 +35,19 @@ int check_path(char *dirName)
     }
     return 1;
 }
+int check_directory(char *dirName)
+{
+    // Check if realy a directory
+    struct stat fileMetadata;
+
+    if (stat(dirName, &fileMetadata) < 0)
+        return -1;
+
+    if (!S_ISDIR(fileMetadata.st_mode))
+        return -1;
+
+    return 1;
+}
 
 void list_directory(char *dirName)
 {
@@ -91,6 +104,8 @@ void list_directory_filtered_perm(char *dirName, char *filter)
     DIR *dir;
     struct dirent *dirEntry;
     struct stat fileMetadata;
+    char *permision = malloc(MAX_PATH_LEN * sizeof(char));
+    char *path = malloc(MAX_PATH_LEN * sizeof(char));
 
     dir = opendir(dirName);
     if (dir == 0)
@@ -98,18 +113,265 @@ void list_directory_filtered_perm(char *dirName, char *filter)
         printf("%s", "ERROR\nerror opening directory\n");
         exit(4);
     }
-    
+
     // iterate the directory contents
     while ((dirEntry = readdir(dir)) != 0)
     {
         // build the complete path to the element in the directory
         if (strcmp(dirEntry->d_name, ".") != 0 && strcmp(dirEntry->d_name, "..") != 0)
         {
+            strcpy(path, dirName);
+            strcat(path, "/");
+            strcat(path, dirEntry->d_name);
+            // printf("%s\n",path);
+
+            if (stat(path, &fileMetadata) < 0)
+            { // get info about a file system element (file, directory etc.)
+                printf("ERROR\ngetting info about the file\n)");
+                exit(2);
+            }
+            else
+            {
+                if (fileMetadata.st_mode & S_IRUSR) // check owner's READ permission (S_IRUSR is the mask 0400)
+                    strcat(permision, "r");
+                else
+                    strcat(permision, "-");
+
+                if (fileMetadata.st_mode & S_IWUSR) // check owner's WRITE permission (S_IWUSR is the mask 0200)
+                    strcat(permision, "w");
+                else
+                    strcat(permision, "-");
+
+                if (fileMetadata.st_mode & S_IXUSR) // check owner's EXECUTION permission (S_IXUSR is the mask 0100)
+                    strcat(permision, "x");
+                else
+                    strcat(permision, "-");
+
+                if (fileMetadata.st_mode & S_IRGRP) // check group's READ permission (S_IRGRP is the mask 0040)
+                    strcat(permision, "r");
+                else
+                    strcat(permision, "-");
+
+                if (fileMetadata.st_mode & S_IWGRP) // check group's WRITE permission (S_IWGRP is the mask 0020)
+                    strcat(permision, "w");
+                else
+                    strcat(permision, "-");
+
+                if (fileMetadata.st_mode & S_IXGRP) // check group's EXECUTION permission (S_IXGRP is the mask 0010)
+                    strcat(permision, "x");
+                else
+                    strcat(permision, "-");
+
+                if (fileMetadata.st_mode & S_IROTH) // check others' READ permission (S_IROTH is the mask 0004)
+                    strcat(permision, "r");
+                else
+                    strcat(permision, "-");
+
+                if (fileMetadata.st_mode & S_IWOTH) // check others' WRITE permission (S_IWOTH is the mask 0002)
+                    strcat(permision, "w");
+                else
+                    strcat(permision, "-");
+
+                if (fileMetadata.st_mode & S_IXOTH) // check others' EXECUTION permission (S_IXOTH is the mask 0001)
+                    strcat(permision, "x");
+                else
+                {
+                    strcat(permision, "-");
+                }
+                if (strcmp(permision, filter) == 0)
+                {
+                    printf("%s/%s", dirName, dirEntry->d_name);
+                    printf("\n");
+                }
+                strcpy(permision, "");
+            }
+        }
+    }
+
+    free(permision);
+    free(path);
+    closedir(dir);
+}
+void list_directory_rec(char *dirName, int *dirPrinted)
+{
+    DIR *dir;
+    struct dirent *dirEntry;
+    char *path = malloc(MAX_PATH_LEN * sizeof(char));
+    dir = opendir(dirName);
+    if (dir == 0)
+    {
+        printf("%s", "ERROR\nerror opening directory\n");
+        exit(4);
+    }
+    if (!(*dirPrinted))
+    {
+        printf("%s\n", dirName);
+        *dirPrinted = 1;
+    }
+    // iterate the directory contents
+    while ((dirEntry = readdir(dir)) != 0)
+    {
+        // build the complete path to the element in the directory
+        if (strcmp(dirEntry->d_name, ".") != 0 && strcmp(dirEntry->d_name, "..") != 0)
+        {
+            strcat(path, dirName);
+            strcat(path, "/");
+            strcat(path, dirEntry->d_name);
+            if (check_directory(path) == 1)
+            {
+                list_directory_rec(path, dirPrinted);
+            }
             printf("%s/%s", dirName, dirEntry->d_name);
             printf("\n");
         }
     }
 
+    free(path);
+    closedir(dir);
+}
+void list_directory_rec_filtered_name(char *dirName, int *dirPrinted, char *filter)
+{
+    DIR *dir;
+    struct dirent *dirEntry;
+    char *path = malloc(MAX_PATH_LEN * sizeof(char));
+    dir = opendir(dirName);
+    if (dir == 0)
+    {
+        printf("%s", "ERROR\nerror opening directory\n");
+        exit(4);
+    }
+    if (!(*dirPrinted) && string_ends_with(dirName, filter))
+    {
+        printf("%s\n", dirName);
+        *dirPrinted = 1;
+    }
+    // iterate the directory contents
+    while ((dirEntry = readdir(dir)) != 0)
+    {
+        // build the complete path to the element in the directory
+        if (strcmp(dirEntry->d_name, ".") != 0 && strcmp(dirEntry->d_name, "..") != 0)
+        {
+            strcat(path, dirName);
+            strcat(path, "/");
+            strcat(path, dirEntry->d_name);
+            if (check_directory(path) == 1)
+            {
+                *dirPrinted = 1;
+                list_directory_rec_filtered_name(path, dirPrinted, filter);
+            }
+            if (string_ends_with(dirEntry->d_name, filter))
+            {
+                printf("%s/%s", dirName, dirEntry->d_name);
+                printf("\n");
+            }
+        }
+    }
+
+    free(path);
+    closedir(dir);
+}
+void list_directory_rec_filtered_perm(char *dirName, int *dirPrinted, char *filter)
+{
+    DIR *dir;
+    struct dirent *dirEntry;
+    struct stat fileMetadata;
+    char *permision = malloc(MAX_PATH_LEN * sizeof(char));
+    char *path = malloc(MAX_PATH_LEN * sizeof(char));
+
+    dir = opendir(dirName);
+    if (dir == 0)
+    {
+        printf("%s", "ERROR\nerror opening directory\n");
+        exit(4);
+    }
+
+    // iterate the directory contents
+    while ((dirEntry = readdir(dir)) != 0)
+    {
+        // build the complete path to the element in the directory
+        if (strcmp(dirEntry->d_name, ".") != 0 && strcmp(dirEntry->d_name, "..") != 0)
+        {
+            strcpy(path, dirName);
+            strcat(path, "/");
+            strcat(path, dirEntry->d_name);
+            if (check_directory(path) == 1)
+            {
+                *dirPrinted = 1;
+                list_directory_rec_filtered_perm(path, dirPrinted, filter);
+            }
+            // printf("%s\n",path);
+
+            if (stat(path, &fileMetadata) < 0)
+            { // get info about a file system element (file, directory etc.)
+                printf("ERROR\ngetting info about the file\n)");
+                exit(2);
+            }
+            else
+            {
+                if (fileMetadata.st_mode & S_IRUSR) // check owner's READ permission (S_IRUSR is the mask 0400)
+                    strcat(permision, "r");
+                else
+                    strcat(permision, "-");
+
+                if (fileMetadata.st_mode & S_IWUSR) // check owner's WRITE permission (S_IWUSR is the mask 0200)
+                    strcat(permision, "w");
+                else
+                    strcat(permision, "-");
+
+                if (fileMetadata.st_mode & S_IXUSR) // check owner's EXECUTION permission (S_IXUSR is the mask 0100)
+                    strcat(permision, "x");
+                else
+                    strcat(permision, "-");
+
+                if (fileMetadata.st_mode & S_IRGRP) // check group's READ permission (S_IRGRP is the mask 0040)
+                    strcat(permision, "r");
+                else
+                    strcat(permision, "-");
+
+                if (fileMetadata.st_mode & S_IWGRP) // check group's WRITE permission (S_IWGRP is the mask 0020)
+                    strcat(permision, "w");
+                else
+                    strcat(permision, "-");
+
+                if (fileMetadata.st_mode & S_IXGRP) // check group's EXECUTION permission (S_IXGRP is the mask 0010)
+                    strcat(permision, "x");
+                else
+                    strcat(permision, "-");
+
+                if (fileMetadata.st_mode & S_IROTH) // check others' READ permission (S_IROTH is the mask 0004)
+                    strcat(permision, "r");
+                else
+                    strcat(permision, "-");
+
+                if (fileMetadata.st_mode & S_IWOTH) // check others' WRITE permission (S_IWOTH is the mask 0002)
+                    strcat(permision, "w");
+                else
+                    strcat(permision, "-");
+
+                if (fileMetadata.st_mode & S_IXOTH) // check others' EXECUTION permission (S_IXOTH is the mask 0001)
+                    strcat(permision, "x");
+                else
+                {
+                    strcat(permision, "-");
+                }
+                if (!(*dirPrinted) && strcmp(permision, filter) == 0)
+                {
+                    printf("%s\n", dirName);
+                    *dirPrinted = 1;
+                }
+
+                if (strcmp(permision, filter) == 0)
+                {
+                    printf("%s/%s", dirName, dirEntry->d_name);
+                    printf("\n");
+                }
+                strcpy(permision, "");
+            }
+        }
+    }
+
+    free(permision);
+    free(path);
     closedir(dir);
 }
 
@@ -123,6 +385,7 @@ int main(int argc, char **argv)
         }
         else if (strcmp(argv[1], "list") == 0)
         {
+            int dirPrinted = 0;
             char *path = malloc(MAX_PATH_LEN * sizeof(char));
             char *op_name = malloc(MAX_PATH_LEN * sizeof(char));
             char *op_perm = malloc(MAX_PATH_LEN * sizeof(char));
@@ -132,25 +395,53 @@ int main(int argc, char **argv)
                 {
                     if (argc >= 4)
                     {
-                        printf("%s", "SUCCESS\n");
-                        // TODO: If the “recursive” option is also specified, your program should traverse the
-                        // entire sub-tree starting from the given directory, entering recursively in all the
-                        // sub-folders, sub-sub-folders and so on. Found element names must also contain
-                        // their path relative to the given directory. The two boxes below illustrate the
-                        // way the program could be run recursively and a possible output for that case,
-                        // respectively.
-                        if (argc == 4)
-                            printf("%s", "list recursive path\n");
+                        if (argc == 4) // list recursive path
+                        {
+                            strncpy(path, argv[3] + 5, strlen(argv[3]));
+                            if (check_path(path) == 1)
+                            {
+                                printf("%s", "SUCCESS\n");
+                                list_directory_rec(path, &dirPrinted);
+                            }
+                        }
                         else
                         {
                             char *ptr = strstr(argv[3], "path");
-                            if (ptr != NULL)
+                            if (ptr != NULL) // list recursive path operation
                             {
-                                printf("%s", "list recursive path operation\n");
+                                strncpy(path, argv[3] + 5, strlen(argv[3]));
+                                if (check_path(path) == 1)
+                                {
+                                    printf("%s", "SUCCESS\n");
+                                    if (strstr(argv[4], "name_ends_with") != NULL) // name_ends_with filter
+                                    {
+                                        strncpy(op_name, argv[4] + 15, strlen(argv[4]));
+                                        list_directory_rec_filtered_name(path, &dirPrinted, op_name);
+                                    }
+                                    else // permissions filter
+                                    {
+                                        strncpy(op_perm, argv[4] + 12, strlen(argv[4]));
+                                        list_directory_rec_filtered_perm(path, &dirPrinted, op_perm);
+                                    }
+                                }
                             }
-                            else
+                            else // list recursive operation path
                             {
-                                printf("%s", "list recursive operation path\n");
+                                strncpy(path, argv[4] + 5, strlen(argv[4]));
+                                if (check_path(path) == 1)
+                                {
+                                    printf("%s", "SUCCESS\n");
+                                    if (strstr(argv[3], "name_ends_with") != NULL) // name_ends_with filter
+                                    {
+                                        strncpy(op_name, argv[3] + 15, strlen(argv[3]));
+                                        list_directory_rec_filtered_name(path, &dirPrinted, op_name);
+                                    }
+                                    else // permissions filter
+                                    {
+                                        strncpy(op_perm, argv[3] + 12, strlen(argv[3]));
+                                        list_directory_rec_filtered_perm(path, &dirPrinted, op_perm);
+                                    }
+                                }
                             }
                         }
                     }
@@ -162,20 +453,55 @@ int main(int argc, char **argv)
                     if (argc >= 4)
                     {
 
-                        printf("%s", "SUCCESS\n");
-
                         if (argc == 4)
-                            printf("%s", "list path recursive\n");
+                        {
+                            // list path recursive
+                            strncpy(path, argv[2] + 5, strlen(argv[2]));
+                            if (check_path(path) == 1)
+                            {
+                                printf("%s", "SUCCESS\n");
+                                list_directory_rec(path, &dirPrinted);
+                            }
+                        }
+
                         else
                         {
                             char *ptr = strstr(argv[2], "path");
-                            if (ptr != NULL)
+                            if (ptr != NULL) // list path operation recursive
                             {
-                                printf("%s", "list path operation recursive\n");
+                                strncpy(path, argv[2] + 5, strlen(argv[2]));
+                                if (check_path(path) == 1)
+                                {
+                                    printf("%s", "SUCCESS\n");
+                                    if (strstr(argv[3], "name_ends_with") != NULL) // name_ends_with filter
+                                    {
+                                        strncpy(op_name, argv[3] + 15, strlen(argv[3]));
+                                        list_directory_rec_filtered_name(path, &dirPrinted, op_name);
+                                    }
+                                    else // permissions filter
+                                    {
+                                        strncpy(op_perm, argv[3] + 12, strlen(argv[3]));
+                                        list_directory_rec_filtered_perm(path, &dirPrinted, op_perm);
+                                    }
+                                }
                             }
-                            else
+                            else // list operation path recursive
                             {
-                                printf("%s", "list operation path recursive\n");
+                                strncpy(path, argv[3] + 5, strlen(argv[3]));
+                                if (check_path(path) == 1)
+                                {
+                                    printf("%s", "SUCCESS\n");
+                                    if (strstr(argv[2], "name_ends_with") != NULL) // name_ends_with filter
+                                    {
+                                        strncpy(op_name, argv[2] + 15, strlen(argv[2]));
+                                        list_directory_rec_filtered_name(path, &dirPrinted, op_name);
+                                    }
+                                    else // permissions filter
+                                    {
+                                        strncpy(op_perm, argv[2] + 12, strlen(argv[2]));
+                                        list_directory_rec_filtered_perm(path, &dirPrinted, op_perm);
+                                    }
+                                }
                             }
                         }
                     }
@@ -185,10 +511,9 @@ int main(int argc, char **argv)
                 else
                 {
 
-                    if (argc == 3)
+                    if (argc == 3) // list path
                     {
                         strncpy(path, argv[2] + 5, strlen(argv[2]));
-                        // printf("%s", "list  path\n");
                         if (check_path(path) == 1)
                         {
                             printf("%s", "SUCCESS\n");
@@ -242,6 +567,34 @@ int main(int argc, char **argv)
             {
                 printf("%s", "ERROR\nnot enough parameters\n");
             }
+            free(path);
+            free(op_name);
+            free(op_perm);
+        }
+        else if (strcmp(argv[1], "parse") == 0 || strcmp(argv[2], "parse") == 0)
+        {
+            int fd;
+            char buffer[100];
+            char *path = malloc(MAX_PATH_LEN * sizeof(char));
+            if (strcmp(argv[1], "parse") == 0)
+                strncpy(path, argv[2] + 5, strlen(argv[2]));
+
+            else if (strcmp(argv[2], "parse") == 0)
+                strncpy(path, argv[1] + 5, strlen(argv[1]));
+
+            fd=open(path,O_RDONLY);
+            if(fd<0)
+            printf("ERROR\nCould not open file\n");
+
+            if(read(fd,buffer,4)<0)
+            printf("ERROR\nCould not read file\n");
+
+            for (int i = 0; i <4; i++)
+            {
+                printf("%c",buffer[i]);
+            }
+
+                free(path);
         }
     }
     return 0;
